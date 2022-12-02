4621facc99df8e6adce3097ca4c41d139d3053f6
sldone <slidonetomsk@gmail.com>
2022-12-02 09:32:51 +0000

    Deploying to gh-pages from @ Arcanite-ui/ui-elements@f53bf3b4b3aad9e14f35ef6de73c4a5e28547235 🚀

--- assets/config.30e6a0eb.js
+++
@@ -1,2 +0,0 @@
-import{d as t,r as m,a as s}from"./decorateStory.1029e4b0.js";import"./iframe.10dedb65.js";import"./vue.esm-bundler.a324432b.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
-//# sourceMappingURL=config.30e6a0eb.js.map
